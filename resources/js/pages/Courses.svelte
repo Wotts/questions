400e1b16118784e875fc23160a02ca58972a1e71
Wotts <work@wotts.nl>
2026-05-14 13:12:02 +0200

    feat(homepage): create empty homepage

---
+++ resources/js/pages/Courses.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import AppHead from '@/components/AppHead.svelte';
+</script>
+
+<AppHead title="Courses" />
+
+<div>
+    <main>
+        Hello, Learnbeat.
+    </main>
+</div>
